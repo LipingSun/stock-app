95dfd9c2aade18ac8650b294e2992e98af9a6800
LipingSun <liping.sun@sjsu.edu>
2016-05-05 01:54:55 -0700

    move consts to config file

---
+++ app/config.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Liping on 5/5/16.
+ */
+
+// API_URL = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=";
+export const WEB_SERVER = 'http://192.168.1.103:3000';
+export const INTERACTIVE_CHART_API = WEB_SERVER + '/MODApis/Api/v2/InteractiveChart/json?parameters=';
