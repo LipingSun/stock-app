b3af33c2ffb7dfe69c8a04036371e216b3edac7c
LipingSun <liping.sun@sjsu.edu>
2016-05-04 22:56:31 -0700

    show price

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,52 +1,57 @@
-import {Page, NavController, Modal, NavParams} from 'ionic-angular';
-import {Http, Headers} from 'angular2/http';
+import {Page, NavController, Modal} from 'ionic-angular';
+import {Http} from 'angular2/http';
 import {CHART_DIRECTIVES} from 'angular2-highcharts';
-import 'rxjs/add/operator/map'
 import {SearchPage} from '../search-page/search-page';
 
 // API_URL = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=";
-// API_URL = "http://10.250.38.241:3000/MODApis/Api/v2/InteractiveChart/json?parameters=";
-const API_ENDPOINT = "http://192.168.1.103:3000/MODApis/Api/v2/InteractiveChart/json?parameters=";
+const WEB_SERVER = 'http://localhost:3000';
+const INTERACTIVE_CHART_API = WEB_SERVER + '/MODApis/Api/v2/InteractiveChart/json?parameters=';
 
 @Page({
     templateUrl: 'build/pages/page1/page1.html',
     directives: [CHART_DIRECTIVES]
 })
 export class Page1 {
-    searchQuery:string = '';
-    items = ["Apple", "Google"];
-    isListHidden = true;
-    options:Object;
+    stock = {
+        symbol: null,
+        chartOption: null
+    };
+    compareList = [];
     hideChart = true;
     symbol:String;
+    isCompare:boolean = false;
 
     constructor(public http:Http, public nav:NavController) {
     }
 
     onPageWillEnter() {
-        if (this.symbol) {
-            console.log(this.symbol);
-            this.getHistoryData(this.symbol);
-            this.hideChart = false;
+        if (this.stock.symbol) {
+            if (!this.isCompare) {
+                console.log(this.stock.symbol);
+                this.getHistoryData(this.stock);
+                this.hideChart = false;
+            } else {
+
+            }
         }
     }
 
-    openSearchPage() {
-        this.nav.push(SearchPage, this);
+    openSearchPage(stock) {
+        this.nav.push(SearchPage, stock);
         // let modal = Modal.create(SearchPage);
         // this.nav.present(modal);
     }
 
-    getHistoryData(symbol) {
+    getHistoryData(stock) {
         let params = {
             Normalized: false,
-            // NumberOfDays: 365,
-            StartDate: '2011-03-01T00:00:00-00',
-            EndDate: '2016-05-02T00:00:00-00',
+            NumberOfDays: 365,
+            // StartDate: '2011-03-01T00:00:00-00',
+            // EndDate: '2016-05-02T00:00:00-00',
             DataPeriod: "Day",
             Elements: [
                 {
-                    Symbol: symbol,
+                    Symbol: stock.symbol,
                     Type: "price",
                     Params: ["ohlc"] //ohlc, c = close only
                 }
@@ -54,20 +59,30 @@ export class Page1 {
             //,LabelPeriod: 'Week',
             //LabelInterval: 1
         };
-        let url = API_ENDPOINT + JSON.stringify(params);
+        let url = INTERACTIVE_CHART_API + JSON.stringify(params);
         console.log(url);
         this.http.get(url).subscribe(
             res => {
                 console.log("ok", res.json());
-                this.render(symbol, res.json());
+                this.setChartOption(stock.symbol, res.json());
             },
             err => {
                 console.log("err", err.json());
             }
         );
+        // this.http.get('http://192.168.1.103:3000/api/compare/3ced5627-44ad-4683-99dc-62779c9dd9c4').subscribe(
+        //     res => {
+        //         console.log("ok", res.json());
+        //         this.render(symbol, res.json());
+        //     },
+        //     err => {
+        //         console.log("err", err.json());
+        //     }
+        // );
     }
 
-    render(symbol, data) {
+    setChartOption(symbol, data) {
+        // data = data.body.stockB;
         var dates = data.Dates || [];
         var elements = data.Elements || [];
         var chartSeries = [];
@@ -79,15 +94,15 @@ export class Page1 {
                 var dat = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
                 var pointData = [
                     dat,
-                    elements[0].DataSeries['open'].values[i],
-                    elements[0].DataSeries['high'].values[i],
-                    elements[0].DataSeries['low'].values[i],
+                    // elements[0].DataSeries['open'].values[i],
+                    // elements[0].DataSeries['high'].values[i],
+                    // elements[0].DataSeries['low'].values[i],
                     elements[0].DataSeries['close'].values[i]
                 ];
                 chartSeries.push(pointData);
             }
 
-            this.options = {
+            this.stock.chartOption = {
                 rangeSelector: {
                     selected: 0
                 },
@@ -114,23 +129,9 @@ export class Page1 {
         }
     }
 
-    getItems(searchbar) {
-
-        this.isListHidden = false;
-
-        // set q to the value of the searchbar
-        var q = searchbar.value;
-
-        // if the value is an empty string don't filter the items
-        if (q.trim() == '') {
-            return;
-        }
-
-        this.items = this.items.filter((v) => {
-            if (v.toLowerCase().indexOf(q.toLowerCase()) > -1) {
-                return true;
-            }
-            return false;
-        })
+    compare() {
+        
+        this.compareList.push();
+        // this.openSearchPage();
     }
 }
