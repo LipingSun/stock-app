5b0a5f0170fe42243e8c4a5402af104c154f289f
LipingSun <liping.sun@sjsu.edu>
2016-05-05 02:40:52 -0700

    add LOOKUP_API

--- app/config.ts
+++ app/config.ts
@@ -4,4 +4,5 @@
 
 // API_URL = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=";
 export const WEB_SERVER = 'http://192.168.1.103:3000';
+export const LOOKUP_API = WEB_SERVER + '/MODApis/Api/v2/Lookup/json?input=';
 export const INTERACTIVE_CHART_API = WEB_SERVER + '/MODApis/Api/v2/InteractiveChart/json?parameters=';
