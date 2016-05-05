a12114033c1d1a06b1b73a76cd77f6025c1e0643
LipingSun <liping.sun@sjsu.edu>
2016-05-05 02:40:37 -0700

    implement search

--- app/pages/search-page/search-page.ts
+++ app/pages/search-page/search-page.ts
@@ -1,20 +1,41 @@
 import {Page, NavController, NavParams} from 'ionic-angular';
+import {Http} from "angular2/http";
+import {LOOKUP_API} from "../../config";
 
-/*
- Generated class for the Search page.
-
- See http://ionicframework.com/docs/v2/components/#navigation for more info on
- Ionic pages and navigation.
- */
 @Page({
     templateUrl: 'build/pages/search-page/search-page.html',
 })
 export class SearchPage {
-    constructor(public nav:NavController, public navParams:NavParams) {
+    items;
+
+    constructor(public nav:NavController, public navParams:NavParams, public http:Http) {
+        console.log(this.navParams.data);
     }
 
-    selectStock() {
-        this.navParams.data.symbol = 'AAPL';
+    selectStock(symbol) {
+        this.navParams.data.symbol = symbol;
         this.nav.pop();
+    }
+
+    getItems(searchbar) {
+
+        var keyword = searchbar.value.trim();
+
+        if (keyword == '' || keyword.length < 2) {
+            this.items = [];
+            return;
+        }
+
+        let url = LOOKUP_API + JSON.stringify(keyword);
+        console.log(url);
+        this.http.get(url).subscribe(
+            res => {
+                console.log("ok", res.json());
+                this.items = res.json();
+            },
+            err => {
+                console.log("err", err.json());
+            }
+        );
     }
 }
